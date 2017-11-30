4122a0ab0455d60a903febebb530daa2d29f4583
Byron Varberg <byron@byronvarberg.com>
2017-11-29 23:18:59 -0600

    Use get-video-platform function

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+import getVideoPlatform from './lib/get-video-platform';
+
 const renderTarget = document.getElementById('root');
+
+// TEMPORARY: video URL
+const video = 'https://vimeo.com/215814095';
+
+// Validate the provided URL and determine the video type
+getVideoPlatform(video).then((platform) => {
+  console.log(platform);
+}).catch((err) => {
+  // TODO: render an error to the user, if necessary
+  console.error(err);
+});
 
 render(<div>Transcript</div>, renderTarget);
